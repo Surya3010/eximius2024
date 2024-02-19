f6663803e4dd1c6dc09400968fbbbbb512ccc747
Surya Purushothaman <surya@Suryas-MacBook-Pro.local>
2024-02-19 08:08:58 +0530

    fix: build issue

--- components/Common/Intro/index.jsx
+++ components/Common/Intro/index.jsx
@@ -8,7 +8,7 @@ import Tools from './Tools';
 import Skills from './Skills';
 import Image from 'next/image';
 import { NAME, DESIGNATION, SOCIAL_LINKS } from '../../../constants/constants';
-import ProfilePic from '../../../public/images/profile.jpg'
+import ProfilePic from '../../../public/images/profile.jpeg'
 
 const Intro = () => {
   return (
