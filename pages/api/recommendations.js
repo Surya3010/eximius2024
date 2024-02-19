039d18edaa793902d1b095c5e4f7399a9dbabed1
Surya Purushothaman <surya@suryas-mbp.iimb.ernet.in>
2024-02-19 09:34:46 +0530

    fix: cleanup recommendations

--- pages/api/recommendations.js
+++ pages/api/recommendations.js
@@ -3,32 +3,32 @@ const recommendationCard = [
   {
     id: 0,
     name: 'Karthikeyan',
-    image: "images/shahmir.jfif",
+    image: "images/karthikeyan.jfif",
     designation: 'React JS | NEXT JS | Node | GraphQL',
     view: "Hey everyone! I wanted to take a moment to recommend Surya for any React.js opportunities. I had the pleasure of working with him on multiple projects where he showcased a strong understanding of React.js and Next.js concepts and delivered high-quality code. He consistently demonstrated a willingness to learn and grow, and his enthusiasm for tackling new challenges was contagious. Surya is a reliable and dedicated team member who would be a great asset to any React.js team. Highly recommended! 👍! 🌟",
     linkednURL: "https://www.linkedin.com"
   },
   {
     id: 1,
-    name: 'Sikandar Hayat',
-    image: "images/sikandar.jpeg",
+    name: 'Aravind',
+    image: "images/aravind.jpeg",
     designation: 'Data Scientist | Machine Learning Engineer | Python Developer',
     view: "I had the privilege of working alongside Surya on multiple university projects, and I must say, his talent and work ethic truly stood out. Recently, we were both part of the dynamic team at JMM Technologies, where Surya's professionalism and attention to detail shone brightly. I wholeheartedly endorse Surya for any opportunity that calls for a highly skilled and dedicated individual.",
-    linkednURL: "https://www.linkedin.com/in/sikandar-hayat-381407179/"
+    linkednURL: "https://www.linkedin.com/in/"
   },
   {
     id: 2,
-    name: 'Nasir Khan',
-    image: "images/nasir.jpg",
+    name: 'Kishore',
+    image: "images/kishore.jpg",
     designation: 'MERN | JavaScript | Tailwind | Sass | Bootstrap',
     view: "I wholeheartedly recommend Surya as a talented React frontend developer with an incredible flair for UI/UX design. His proficiency in Node.js further enhances his capabilities, allowing him to build robust and scalable applications. Surya's attention to detail, problem-solving skills, and dedication to delivering exceptional results make him a valuable addition to any development team.",
-    linkednURL: "https://www.linkedin.com/in/nasirkhan22/"
+    linkednURL: "https://www.linkedin.com/in/"
   },
   {
     id: 3,
     name: 'Naveen Kumar',
-    image: "images/muhammad.jpeg",
-    designation: 'MERN Stack Developer at Productbox',
+    image: "images/naveen.jpeg",
+    designation: 'MERN Stack Developer at Cognizant',
     view: "I highly recommend Surya for web frontend development positions. Their expertise in ReactJS and Next.js, combined with their professionalism and dedication, make them an invaluable asset to any team.",
     linkednURL: "https://www.linkedin.com"
   },
--- pages/api/review.js
+++ pages/api/review.js
@@ -1,14 +1,14 @@
 const review = [
     {
         id: 0,
-        clientName: 'Mustafash',
-        clientLocation: 'Egypt',
+        clientName: 'Brian',
+        clientLocation: 'United Kingdom',
         clientSource: 'Fiverr',
         clientReview: 'It is always nice to work with him. The work is finished quickly and he does more than is asked of him. It is also clear that he has a lot of knowledge of the work he does. I look forward to our next collaboration.'
     },
     {
         id: 2,
-        clientName: 'Melissabrown255',
+        clientName: 'Melissa Brown',
         clientLocation: 'United States',
         clientSource: 'Fiverr',
         clientReview: 'He is very good with communication and experienced website developer, great experience to work with him. '
