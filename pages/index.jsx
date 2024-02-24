10a090dc0b2c153e2f384c6753fb34cf389f7630
Surya Purushothaman <surya@Suryas-MacBook-Pro.local>
2024-02-25 04:48:51 +0530

    feat(speakers): add basic speaker container

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,13 @@
 import LandingPage from '../components/LandingPage';
 import AboutTheme from '../components/About/AboutTheme';
 import Yep from '../components/Yep';
+import Speakers from '../components/Speakers';
 const home = () => {
     return (
         <div className="Home-Page -z-10">
             <LandingPage />
             <AboutTheme />
+            <Speakers/>
             <Yep />
         </div>
     )
