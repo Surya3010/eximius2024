39dd33018a3e427db1d6f4ab7870ecea22bc54a3
Surya Purushothaman <surya@Suryas-MacBook-Pro.local>
2024-03-01 05:15:07 +0530

    feat(pastspeakers): add animations effects to icons

--- pages/api/past_speakers.js
+++ pages/api/past_speakers.js
@@ -2,9 +2,9 @@ const review = [
     'images/past_speakers/ankur_warikoo.png',
    'images/past_speakers/ganesh_prasad.png',
     'images/past_speakers/varun_mayya.png',
-    'images/past_speakers/kiran_bedi.png',
-    'images/past_speakers/vetri_subramanian.png',
     'images/past_speakers/arjun_manjumdar.png',
+    'images/past_speakers/vetri_subramanian.png',
+    'images/past_speakers/kiran_bedi.png',
     'images/past_speakers/aniketh_jain.png',
     'images/past_speakers/nandita_sharma.png'
 ]
