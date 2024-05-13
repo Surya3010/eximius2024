3d2b4d4d04001552e56a9630fd7a89612a13b0d0
Surya Purushothaman <surya@Suryas-MacBook-Pro.local>
2024-05-14 00:48:58 +0530

    fix(sponsor): remove sponsor amount

--- pages/sponsors.jsx
+++ pages/sponsors.jsx
@@ -5,7 +5,7 @@ const Yep = () => {
     <Container>
       <div className="h-[230px] w-full bg-previous_sponsors bg-cover bg-custom md:h-[630px] lg:bg-cover lg:bg-left" style={{ borderRadius: '16px', margin: '32px 0px' }} />
       <div className="h-[230px] w-full bg-sponsors_2 bg-cover bg-custom md:h-[630px] lg:bg-cover lg:bg-left" style={{ borderRadius: '16px', margin: '32px 0px' }} />
-      <div className="h-[230px] w-full bg-sponsors_3 bg-cover bg-custom md:h-[630px] lg:bg-cover lg:bg-left" style={{ borderRadius: '16px', margin: '32px 0px' }} />
+      {/* <div className="h-[230px] w-full bg-sponsors_3 bg-cover bg-custom md:h-[630px] lg:bg-cover lg:bg-left" style={{ borderRadius: '16px', margin: '32px 0px' }} /> */}
       <div className="h-[230px] w-full bg-sponsors_4 bg-cover bg-custom md:h-[630px] lg:bg-cover lg:bg-left" style={{ borderRadius: '16px', margin: '32px 0px' }} />
       <div className="h-[230px] w-full bg-sponsors_5 bg-cover bg-custom md:h-[630px] lg:bg-cover lg:bg-left" style={{ borderRadius: '16px', margin: '32px 0px' }} />
     </Container>
