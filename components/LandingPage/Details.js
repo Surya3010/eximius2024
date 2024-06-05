bab030a1c1918b5263b9d18c276483fc7c64f43b
Surya3010 <chandana.karanam23@iimb.ac.in>
2024-06-05 16:11:28 +0530

    modified:   components/LandingPage/Details.js

--- components/LandingPage/Details.js
+++ components/LandingPage/Details.js
@@ -6,7 +6,7 @@ export default function Details() {
   return (
     <article className="mb-24 flex w-full -translate-y-[50px] flex-col items-center justify-center gap-24 text-center md:translate-y-0 lg:mb-32 lg:mt-[75px] lg:flex-row lg:gap-0 ">
       <div className="flex-1 text-[60px] font-extralight leading-[90%] lg:text-left">
-        EXIM<RedText>IUS</RedText>
+        <RedText>EXIMIUS</RedText>
       </div>
       <div className="text-shape flex-1 select-none text-center font-extralight leading-none">
         <div className="text-[80px]">6-7</div>
