26db6fa181145eee352aab65b0662644a7f690a0
Surya3010 <chandana.karanam23@iimb.ac.in>
2024-06-05 22:28:27 +0530

    modified:   components/LandingPage/Details.js

--- components/LandingPage/Details.js
+++ components/LandingPage/Details.js
@@ -5,12 +5,12 @@ import RedText from "../utility/RedText";
 export default function Details() {
   return (
     <article className="mb-24 flex w-full -translate-y-[50px] flex-col items-center justify-center gap-24 text-center md:translate-y-0 lg:mb-32 lg:mt-[75px] lg:flex-row lg:gap-0 ">
-      <div className="flex-1 text-[60px] font-extralight leading-[90%] lg:text-left">
+      <div className="flex-1 text-[60px] font-extralight leading-[90%] lg:text-left" color="white">
         EXIMIUS
       </div>
-      <div className="text-shape flex-1 select-none text-center font-extralight leading-none">
-        <div className="text-[80px]">6-7</div>
-        <div className="text-[30px]">JUL '24</div>
+      <div className="text-shape flex-1 select-none text-center font-extralight leading-none" style= "background-color: #C32630">
+        <div className="text-[80px]" color="white">6-7</div>
+        <div className="text-[30px]" color="white">JUL '24</div>
       </div>
       <div className="flex-1 text-[30px] font-extralight leading-none lg:text-right">
         <div className="text-[30px] flex items-center justify-center">
