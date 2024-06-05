b3f99db7d2b608795b05aa5d5825f8ee7977c425
Surya3010 <chandana.karanam23@iimb.ac.in>
2024-06-05 21:00:02 +0530

    modified:   components/Navbar/index.js

--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -26,24 +26,13 @@ export default function Navbar({ home = true }) {
               alt="EXIMIUS, IIMB"
             />
           </a>
-        </span>
-        <div className="lg:hidden" onClick={toggleClose}>
-          <Hamburger close={close} />
-        </div>
-        <ul className="hidden gap-12 lg:flex">
-          {home ? <Links /> : <HomeLink />}
-        </ul>
-        <div class="hidden lg:block"><button class="pointer-events w-[165px] select-none rounded-[3px] bg-grey font-semibold text-white "><a class="flex h-full w-full justify-center px-4 py-2 lg:py-1" target="_blank" href="https://unstop.com/college-fests/eximius-2024-iim-bangalores-entrepreneurship-summit-indian-institute-of-management-iim-bangalore-245251">Register Now</a></button></div>
-      </div>
-      <div className="absolute left-0 top-0 z-50 flex h-[75px] w-full flex-row items-center justify-between px-3 lg:justify-around" style={{ background: 'black' }}>
-        <span>  
           <a href="https://nsrcel.org/">
             <Image
               src={NSRCELLogo}
               height={65}
               width={65}
               loading="eager"
-              alt="EXIMIUS, IIMB"
+              alt="NSRCEL,EXIMIUS,IIMB"
             />
           </a>
         </span>
@@ -55,6 +44,7 @@ export default function Navbar({ home = true }) {
         </ul>
         <div class="hidden lg:block"><button class="pointer-events w-[165px] select-none rounded-[3px] bg-grey font-semibold text-white "><a class="flex h-full w-full justify-center px-4 py-2 lg:py-1" target="_blank" href="https://unstop.com/college-fests/eximius-2024-iim-bangalores-entrepreneurship-summit-indian-institute-of-management-iim-bangalore-245251">Register Now</a></button></div>
       </div>
+      
       {/* MOBILE NAV */}
       <div
         className={`sticky left-0 top-0 z-40 flex h-screen w-screen flex-col items-center justify-center gap-12 bg-black duration-700 ease-in-out lg:hidden ${close ? "nav-close" : "nav-open"
