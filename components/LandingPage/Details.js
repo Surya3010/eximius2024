4a790903e21f05bb9df7e65d2fdef7bacdbd7720
Surya3010 <chandana.karanam23@iimb.ac.in>
2024-06-06 20:17:07 +0530

    modified:   components/LandingPage/Details.js

--- components/LandingPage/Details.js
+++ components/LandingPage/Details.js
@@ -6,7 +6,7 @@ import './globals.css';
 export default function Details() {
   return (
     <article className="mb-24 flex w-full -translate-y-[50px] flex-col items-center justify-center gap-24 text-center md:translate-y-0 lg:mb-32 lg:mt-[75px] lg:flex-row lg:gap-0 ">
-      <div className="flex-1 text-[60px] font-extralight leading-[90%] lg:text-left" style={myComponentStyle}>
+      <div className="flex-1 text-[60px] leading-[90%] lg:text-left">
         EXIMIUS
       </div>
       <div className="text-shape flex-1 select-none text-center font-extralight leading-none" style={{ backgroundColor: '#C32630' }}>
