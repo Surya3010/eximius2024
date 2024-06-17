ac6d378016cb59562a5c3b7e0fe21ac8f51281af
Surya Purushothaman <surya@Suryas-MacBook-Pro.local>
2024-06-17 18:37:10 +0530

    feat: add speaker for 2024

--- pages/api/speaker_sessions.js
+++ pages/api/speaker_sessions.js
@@ -1,25 +1,12 @@
 const speakerSessions = [
     {
         id: 0,
-        image: "images/speaker_sessions/1.webp",
-        url: "#",
-        title: "Ankur Warikoo",
-        // subtitle: 'Travel Plan Competition'
-    },
-    {
-        id: 0,
-        image: "images/speaker_sessions/2.webp",
-        url: "#",
-        title: "curtain Raiser",
-        // subtitle: 'Travel Plan Competition'
-    },
-    {
-        id: 0,
-        image: "images/speaker_sessions/3.webp",
-        url: "#",
-        title: "Varun Mayya",
-        // subtitle: 'Travel Plan Competition'
+        image: "images/speaker_sessions/Speaker_Kiran Mazumdar Shaw.png",
+        url: "",
+        title: "Kiran Mazumdar Shaw",
+     
     }
+
 ]
 export default function handler(req, res) {
     res.status(200).json(speakerSessions)
