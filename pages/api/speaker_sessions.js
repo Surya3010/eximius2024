ca7988fa9bf0b3cd8fa4bd1055e513b1f675b5e8
Surya Purushothaman <surya@Suryas-MacBook-Pro.local>
2024-06-25 19:12:37 +0530

    feat: add new speakers

--- pages/api/speaker_sessions.js
+++ pages/api/speaker_sessions.js
@@ -2,10 +2,21 @@ const speakerSessions = [
     {
         id: 0,
         image: "images/speaker_sessions/Speaker_Kiran Mazumdar Shaw.png",
-        url: "",
         title: "Kiran Mazumdar Shaw",
      
-    }
+    },
+    {
+        id: 0,
+        image: "images/speaker_sessions/Speaker_Kris GopalaKrishnan.png",
+        title: "Kris GopalaKrishnan",
+     
+    },
+    {
+        id: 0,
+        image: "images/speaker_sessions/Speaker_Ashok Kamath.png",
+        title: "Ashok Kamath & Shukla Bose",
+     
+    },
 
 ]
 export default function handler(req, res) {
