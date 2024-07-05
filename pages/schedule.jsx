a1ce7a616029fa123769a7d2ad1c6415b886ab4b
Surya3010 <chandana.karanam23@iimb.ac.in>
2024-07-06 04:23:23 +0530

    modified:   pages/schedule.jsx modified:   public/images/Eximius24 Schedule/1.png modified:   public/images/Eximius24 Schedule/10.png modified:   public/images/Eximius24 Schedule/11.png modified:   public/images/Eximius24 Schedule/12.png modified:   public/images/Eximius24 Schedule/13.png new file:   public/images/Eximius24 Schedule/14.png new file:   public/images/Eximius24 Schedule/15.png new file:   public/images/Eximius24 Schedule/16.png new file:   public/images/Eximius24 Schedule/17.png new file:   public/images/Eximius24 Schedule/18.png new file:   public/images/Eximius24 Schedule/19.png modified:   public/images/Eximius24 Schedule/2.png modified:   public/images/Eximius24 Schedule/3.png modified:   public/images/Eximius24 Schedule/4.png modified:   public/images/Eximius24 Schedule/5.png modified:   public/images/Eximius24 Schedule/6.png modified:   public/images/Eximius24 Schedule/7.png modified:   public/images/Eximius24 Schedule/8.png modified:   public/images/Eximius24 Schedule/9.png

--- pages/schedule.jsx
+++ pages/schedule.jsx
@@ -5,19 +5,26 @@ import CardLayout from "../components/Common/CardLayout";
 const Sponsors = () => {
   return (
     <Container>
-      <img src="/images/Eximius24 Schedule/GR Copy of EXIMIUS 2024 brochure_1.png" style={{ borderRadius: '16px', margin: '32px 0px' }} />
-      <img src="/images/Eximius24 Schedule/GR Copy of EXIMIUS 2024 brochure_2.png" style={{ borderRadius: '16px', margin: '32px 0px' }} />
-      <img src="/images/Eximius24 Schedule/GR Copy of EXIMIUS 2024 brochure_3.png" style={{ borderRadius: '16px', margin: '32px 0px' }} />
-      <img src="/images/Eximius24 Schedule/GR Copy of EXIMIUS 2024 brochure_4.png" style={{ borderRadius: '16px', margin: '32px 0px' }} />
-      <img src="/images/Eximius24 Schedule/GR Copy of EXIMIUS 2024 brochure_5.png" style={{ borderRadius: '16px', margin: '32px 0px' }} />
-      <img src="/images/Eximius24 Schedule/GR Copy of EXIMIUS 2024 brochure_6.png" style={{ borderRadius: '16px', margin: '32px 0px' }} />
-      <img src="/images/Eximius24 Schedule/GR Copy of EXIMIUS 2024 brochure_7.png" style={{ borderRadius: '16px', margin: '32px 0px' }} />
+      <img src="/images/Eximius24 Schedule/1.png" style={{ borderRadius: '16px', margin: '32px 0px' }} />
+      <img src="/images/Eximius24 Schedule/2.png" style={{ borderRadius: '16px', margin: '32px 0px' }} />
+      <img src="/images/Eximius24 Schedule/3.png" style={{ borderRadius: '16px', margin: '32px 0px' }} />
+      <img src="/images/Eximius24 Schedule/4.png" style={{ borderRadius: '16px', margin: '32px 0px' }} />
+      <img src="/images/Eximius24 Schedule/5.png" style={{ borderRadius: '16px', margin: '32px 0px' }} />
+      <img src="/images/Eximius24 Schedule/6.png" style={{ borderRadius: '16px', margin: '32px 0px' }} />
+      <img src="/images/Eximius24 Schedule/7.png" style={{ borderRadius: '16px', margin: '32px 0px' }} />
       <img src="/images/Eximius24 Schedule/8.png" style={{ borderRadius: '16px', margin: '32px 0px' }} />
       <img src="/images/Eximius24 Schedule/9.png" style={{ borderRadius: '16px', margin: '32px 0px' }} />
       <img src="/images/Eximius24 Schedule/10.png" style={{ borderRadius: '16px', margin: '32px 0px' }} />
       <img src="/images/Eximius24 Schedule/11.png" style={{ borderRadius: '16px', margin: '32px 0px' }} />
       <img src="/images/Eximius24 Schedule/12.png" style={{ borderRadius: '16px', margin: '32px 0px' }} />
       <img src="/images/Eximius24 Schedule/13.png" style={{ borderRadius: '16px', margin: '32px 0px' }} />
+      <img src="/images/Eximius24 Schedule/14.png" style={{ borderRadius: '16px', margin: '32px 0px' }} />
+      <img src="/images/Eximius24 Schedule/15.png" style={{ borderRadius: '16px', margin: '32px 0px' }} />
+      <img src="/images/Eximius24 Schedule/16.png" style={{ borderRadius: '16px', margin: '32px 0px' }} />
+      <img src="/images/Eximius24 Schedule/17.png" style={{ borderRadius: '16px', margin: '32px 0px' }} />
+      <img src="/images/Eximius24 Schedule/18.png" style={{ borderRadius: '16px', margin: '32px 0px' }} />
+      <img src="/images/Eximius24 Schedule/19.png" style={{ borderRadius: '16px', margin: '32px 0px' }} />
+
     </Container>
   );
 };
